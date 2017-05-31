f231fda80b768c4bcdb3493aa56f35418c7d0204
gurtn08 <gurtn08@naver.com>
2017-05-31 09:25:10 +0900

    standardAsset import

---
+++ ProjectGame/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+
+
+        private void Awake()
+        {
+            Debug.Log("Awake");
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+        private void OnEnable()
+        {
+            Debug.Log("Enable");
+        }
+        private void Start()
+        {
+            Debug.Log("Start");
+        }
+
+        private void LateUpdate()
+        {
+            Debug.Log("LateUpdate");
+        }
+
+
+        private void Update()
+        {
+            Debug.Log("Update");
+            if (!m_Jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            Debug.Log("FixedUpdate");
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            m_Character.Move(h, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
